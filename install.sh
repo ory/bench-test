ed23feb234945c6385955c2679449d6afc2f7a1b
aeneasr <3372410+aeneasr@users.noreply.github.com>
2023-06-11 20:07:22 +0200

    u

--- install.sh
+++ install.sh
@@ -32,3 +32,16 @@ git clone https://github.com/aeneasr/hey.git $HOME/hey
 cd $HOME/hey
 git checkout cookie
 go install .
+
+# Docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
